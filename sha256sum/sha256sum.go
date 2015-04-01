2898444fc3891f828fc70b44e2da94a10df140d5
Souleiman <souleiman@hxdcml.com>
2015-04-01 10:31:17 -0400

    added all utility

--- sha256sum/sha256sum.go
+++ sha256sum/sha256sum.go
@@ -5,18 +5,18 @@ import (
     "github.com/souleiman/checksum_util/helper"
     "github.com/docopt/docopt-go"
     "github.com/souleiman/checksum"
-    "crypto/md5"
+    "crypto/sha256"
 )
 
 func main() {
-    name := "md5sum"
+    name := "sha56sum"
     usage := fmt.Sprintf(helper.Usage, name, name, name)
 
     args, _ := docopt.Parse(usage, nil, true, fmt.Sprintf("%s 1.0", name), false)
 
     files := args["<files>"].([]string)
     for _, file := range files {
-        sum, err := checksum.Compute(md5.New(), file)
+        sum, err := checksum.Compute(sha256.New(), file)
 
         if err != nil {
             fmt.Println(err.Error())
